eacd90dfb7818cfa7ea9b25075ea5808cb5b305b
App Academy Student <lxg073000@gmail.com>
2021-09-01 10:33:19 -0400

    update mobile settings

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,7 +1,7 @@
 import Game from "./game";
 
 export default class DOM {
-  constructor() {
+  constructor(device) {
     this.game = new Game();
 
     this.gameWindow = document.defaultView.window;
@@ -15,6 +15,7 @@ export default class DOM {
     this.points = document.getElementById("points");
     this.pointsVal = document.querySelector("#points > p");
     this.targets = document.querySelector("target");
+    this.randomRGB_style = randomRGB_style;
 
     window.logo = this.logo;
     window.background = this.background;
@@ -23,21 +24,19 @@ export default class DOM {
     window.score = this.score;
     window.newColor = newColor;
 
-    this.colorConvert = require("color-convert");
+    const colorConvert = require("color-convert");
 
-    // assign game toggles by default to web browser click
-    this.colorChanger_start = "mousedown";
-    this.colorChanger_end = "mouseup";
-    this.colorChanger_cancel = "";
-
-    // reassigns game toggle to touch if decive browser is mobile
-    document.addEventListener("load", () => {
-      if (mobileAndTabletCheck()) {
-        this.colorChanger_start = "touchstart";
-        this.colorChanger_end = "touchend";
-        this.colorChanger_cancel = "touchcancel";
-      }
-    });
+    if (device) {
+      this.colorChanger_start = "touchstart";
+      this.colorChanger_end = "touchend";
+      this.colorChanger_cancel = "touchcancel";
+      debugger;
+      this.score.style.fontSize = "2rem";
+    } else {
+      this.colorChanger_start = "mousedown";
+      this.colorChanger_end = "mouseup";
+      this.colorChanger_cancel = "";
+    }
 
     //Assign events
 
@@ -106,69 +105,41 @@ export default class DOM {
           pointsBounds.width / 2}px`
       );
     });
-    // document.defaultView.window.addEventListener("load", () => {
-    //   console.log("loaded");
 
-    //   const TVsize = getComputedStyle(this.background).height;
-    //   const targetSize = parseInt(TVsize) - 20;
-
-    //   this.rootStyle.setProperty(
-    //     "--TV-size",
-    //     `${this.background.getBoundingClientRect().height}px`
-    //   );
-    //   this.rootStyle.setProperty(
-    //     "--Score-left",
-    //     `${tvBounds.left + tvBounds.width * 0.802}px`
-    //   );
-    //   this.rootStyle.setProperty(
-    //     "--Score-top",
-    //     `${tvBounds.top + tvBounds.height * 0.504}px`
-    //   );
-    //   this.rootStyle.setProperty(
-    //     "--Points-height",
-    //     `${this.gameBounds.width}px`
-    //   );
-    //   this.rootStyle.setProperty(
-    //     "--Points-width",
-    //     `${this.gameBounds.height}px`
-    //   );
-    // });
-
-    this.leftSwatch.addEventListener(this.colorChanger_start, down);
-    this.leftSwatch.addEventListener(this.colorChanger_end, up);
+    this.leftSwatch.addEventListener(this.colorChanger_start, (e) => {
+      debugger;
+      if (e.which === 1 || e.which === 0) {
+        down();
+      }
+    });
+    this.leftSwatch.addEventListener(this.colorChanger_end, (e) => {
+      if (e.which === 1 || e.which === 0) {
+        up();
+      }
+    });
     //Also clear the interval when user leaves the window with mouse
-    this.leftSwatch.addEventListener(this.colorChanger_cancel, up);
+    this.leftSwatch.addEventListener(this.colorChanger_cancel, (e) => {
+      if (e.which === 1 || e.which === 0) {
+        up();
+      }
+    });
 
     ///----> End Logic for Click & Touch Event <----///
 
     // capture game element and disable context menu
     this.targets.addEventListener("contextmenu", (e) => e.preventDefault());
-    // returns true if client device is mobile.
-    function mobileAndTabletCheck() {
-      let check = false;
-      (function(a) {
-        if (
-          /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
-            a
-          ) ||
-          /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-            a.substr(0, 4)
-          )
-        )
-          check = true;
-      })(navigator.userAgent || navigator.vendor || window.opera);
-      return check;
-    }
 
     ///----> Start Logic for Click & Touch Event <----///
 
     let clickID = -1; //Global ID of mouse down interval
     function down() {
+      document.getElementById("leftSwatch").classList.add("clear");
       if (clickID == -1)
         //Prevent multimple loops!
-        clickID = setInterval(whileDown, 60 /*execute every 1ms*/);
+        clickID = setInterval(whileDown, 60 /*execute every 60ms*/);
     }
     function up() {
+      document.getElementById("leftSwatch").classList.remove("clear");
       if (clickID != -1) {
         //Only stop if exists
         clearInterval(clickID);
@@ -192,16 +163,14 @@ export default class DOM {
         target_rgb_intARR.push(parseInt(val))
       );
 
-      let left_target_hsl = this.colorConvert.rgb.hsl(target_rgb_intARR);
+      let left_target_hsl = colorConvert.rgb.hsl(target_rgb_intARR);
 
       let new_target_color_hsl = [
         left_target_hsl[0] + 10,
         ...left_target_hsl.slice(1),
       ];
 
-      let new_target_color_rgb = this.colorConvert.hsl.rgb(
-        new_target_color_hsl
-      );
+      let new_target_color_rgb = colorConvert.hsl.rgb(new_target_color_hsl);
 
       this.rootStyle.setProperty(
         "--LC",
@@ -209,16 +178,16 @@ export default class DOM {
       );
     };
 
-    const randomRGB_style = () => {
+    function randomRGB_style() {
       let random_0_to_359 =
         Math.round(Math.floor(Math.random() * 359) / 10) * 10;
       let hsl_arr = [random_0_to_359, 100, 50];
 
-      let rgb_arr = this.colorConvert.hsl.rgb(hsl_arr);
+      let rgb_arr = colorConvert.hsl.rgb(hsl_arr);
       let rgb_style = `rgb(${rgb_arr.join(",")})`;
 
       return rgb_style;
-    };
+    }
 
     const convertStyles_RGBtoHSL = (ele) => {
       // takes a document element and returns
@@ -236,7 +205,7 @@ export default class DOM {
 
       // colorConvert imported from NPM library
       // passed array of RGB values returned as HSL
-      let HSLs_array = this.colorConvert.rgb.hsl(RGBs_array);
+      let HSLs_array = colorConvert.rgb.hsl(RGBs_array);
 
       return HSLs_array;
     };
